bdced27435ac46673797efa1a638494a9e3ddca5
Alvin Ye <aye4@stuy.edu>
2018-03-13 19:10:58 -0400

    edited ReadMe, updated QuickSort and results.csv

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -23,23 +23,31 @@ public class QuickSortTester
     public static void main(String[] args)
     {
 	int[] oneK   = QuickSort.buildArray(1000  , 1000);
-	int[] tenK   = QuickSort.buildArray(10000 , 1000);
-	int[] twentyK = QuickSort.buildArray(20000, 1000);
-	int[] thirtyK = QuickSort.buildArray(30000, 1000);
-	int[] fortyK = QuickSort.buildArray(40000, 1000);	
-	int[] fiftyK = QuickSort.buildArray(50000 , 1000);
-	int[] sixtyK = QuickSort.buildArray(60000, 1000);
-	int[] hundredK = QuickSort.buildArray(100000, 1000);
-	
+	int[] tenK   = QuickSort.buildArray(10000 , 10000);
+	int[] twentyK = QuickSort.buildArray(20000, 20000);
+	int[] thirtyK = QuickSort.buildArray(30000, 30000);
+	int[] fortyK = QuickSort.buildArray(40000, 40000);	
+	int[] fiftyK = QuickSort.buildArray(50000 , 50000 );
+	int[] sixtyK = QuickSort.buildArray(60000, 60000);
+	int[] hundredK = QuickSort.buildArray(100000, 100000);
+	int[] oneMil = QuickSort.buildArray(1000000, 1000000);
+	int[] fiveMil =QuickSort.buildArray(5000000, 5000000);
+	int[] tenMil = QuickSort.buildArray(10000000, 10000000);
 
-	time(oneK);
-        time(tenK);
-	time(twentyK);
-	time(thirtyK);
-	time(fortyK);
-	time(fiftyK);
-	time(sixtyK);
-	time(hundredK);
+
+	for(int i = 0; i < 10; i++){
+		time(oneK);
+    	time(tenK);
+		time(twentyK);
+		time(thirtyK);
+		time(fortyK);
+		time(fiftyK);
+		time(sixtyK);
+		time(hundredK);
+		time(oneMil);
+		time(fiveMil);
+		time(tenMil);
+	}
 	
 	
     } // end main method
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ChunkySnail
 APCS2 pd 8, team ChunkySnail (Jessica Wu, Raymond Wu, Alvin Ye), Lab #01: What Does the Data Say?
+
+Hypothesis: We believe that the runtime of quicksort is O(nlogn) <br>
+			We believe that the pivot point will not significantly impact the data <br>
+			We believe that if the data is arranged so that the pivot point is an extreme, then the method will take a bit longer
+
+Background:
+
+Methology: We used System.nanoTime() in order to measure the time it took for each run of quicksort
