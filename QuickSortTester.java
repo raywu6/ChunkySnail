7e82d54d1a71d7abfae63da9b1d4617784f204b4
Alvin Ye <aye4@stuy.edu>
2018-03-13 13:58:26 -0400

    updated all

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -14,7 +14,9 @@ public class QuickSortTester
 	QuickSort.qsort( array);
 	long afterTime = System.nanoTime();
 
-	System.out.println(afterTime - beforeTime);
+	
+	System.out.println(array.length + "," +  (afterTime - beforeTime));
+
 	
     } // end time method
     
@@ -22,11 +24,23 @@ public class QuickSortTester
     {
 	int[] oneK   = QuickSort.buildArray(1000  , 1000);
 	int[] tenK   = QuickSort.buildArray(10000 , 1000);
+	int[] twentyK = QuickSort.buildArray(20000, 1000);
+	int[] thirtyK = QuickSort.buildArray(30000, 1000);
+	int[] fortyK = QuickSort.buildArray(40000, 1000);	
 	int[] fiftyK = QuickSort.buildArray(50000 , 1000);
+	int[] sixtyK = QuickSort.buildArray(60000, 1000);
+	int[] hundredK = QuickSort.buildArray(100000, 1000);
+	
 
 	time(oneK);
         time(tenK);
+	time(twentyK);
+	time(thirtyK);
+	time(fortyK);
 	time(fiftyK);
+	time(sixtyK);
+	time(hundredK);
+	
 	
     } // end main method
 
