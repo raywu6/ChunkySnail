95585e532b528c42f26b3bddf6f300119d15861d
Alvin Ye <aye4@stuy.edu>
2018-03-13 20:31:46 -0400

    added graph (ignoring the outliers), updated results.csv, updated QuickSortTester

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -10,9 +10,9 @@ public class QuickSortTester
 
     public static void time(int[] array)
     {
-	long beforeTime = System.nanoTime();
+	long beforeTime = System.currentTimeMillis();
 	QuickSort.qsort( array);
-	long afterTime = System.nanoTime();
+	long afterTime = System.currentTimeMillis();
 
 	
 	System.out.println(array.length + "," +  (afterTime - beforeTime));
@@ -22,32 +22,89 @@ public class QuickSortTester
     
     public static void main(String[] args)
     {
-	int[] oneK   = QuickSort.buildArray(1000  , 1000);
-	int[] tenK   = QuickSort.buildArray(10000 , 10000);
-	int[] twentyK = QuickSort.buildArray(20000, 20000);
-	int[] thirtyK = QuickSort.buildArray(30000, 30000);
-	int[] fortyK = QuickSort.buildArray(40000, 40000);	
-	int[] fiftyK = QuickSort.buildArray(50000 , 50000 );
-	int[] sixtyK = QuickSort.buildArray(60000, 60000);
-	int[] hundredK = QuickSort.buildArray(100000, 100000);
-	int[] oneMil = QuickSort.buildArray(1000000, 1000000);
-	int[] fiveMil =QuickSort.buildArray(5000000, 5000000);
 	int[] tenMil = QuickSort.buildArray(10000000, 10000000);
+	int[] twentyFiveMil = QuickSort.buildArray(25000000, 25000000);
+	int[] fortyMil = QuickSort.buildArray(40000000, 40000000);
+	int[] fiftyMil = QuickSort.buildArray(50000000, 50000000);
+	int[] seventyFiveMil = QuickSort.buildArray(75000000, 75000000);
+	int[] hundredMil = QuickSort.buildArray(100000000, 100000000);
 
 
-	for(int i = 0; i < 10; i++){
-		time(oneK);
-    	time(tenK);
-		time(twentyK);
-		time(thirtyK);
-		time(fortyK);
-		time(fiftyK);
-		time(sixtyK);
-		time(hundredK);
-		time(oneMil);
-		time(fiveMil);
+
+
 		time(tenMil);
-	}
+		time(twentyFiveMil);
+		time(fortyMil);
+		time(fiftyMil);
+		time(seventyFiveMil);
+		time(hundredMil);
+
+		time(tenMil);
+		time(twentyFiveMil);
+		time(fortyMil);
+		time(fiftyMil);
+		time(seventyFiveMil);
+		time(hundredMil);
+
+		time(tenMil);
+		time(twentyFiveMil);
+		time(fortyMil);
+		time(fiftyMil);
+		time(seventyFiveMil);
+		time(hundredMil);
+
+		time(tenMil);
+		time(twentyFiveMil);
+		time(fortyMil);
+		time(fiftyMil);
+		time(seventyFiveMil);
+		time(hundredMil);
+
+		time(tenMil);
+		time(twentyFiveMil);
+		time(fortyMil);
+		time(fiftyMil);
+		time(seventyFiveMil);
+		time(hundredMil);
+
+		time(tenMil);
+		time(twentyFiveMil);
+		time(fortyMil);
+		time(fiftyMil);
+		time(seventyFiveMil);
+		time(hundredMil);
+
+		time(tenMil);
+		time(twentyFiveMil);
+		time(fortyMil);
+		time(fiftyMil);
+		time(seventyFiveMil);
+		time(hundredMil);
+
+		time(tenMil);
+		time(twentyFiveMil);
+		time(fortyMil);
+		time(fiftyMil);
+		time(seventyFiveMil);
+		time(hundredMil);
+
+		time(tenMil);
+		time(twentyFiveMil);
+		time(fortyMil);
+		time(fiftyMil);
+		time(seventyFiveMil);
+		time(hundredMil);
+
+		time(tenMil);
+		time(twentyFiveMil);
+		time(fortyMil);
+		time(fiftyMil);
+		time(seventyFiveMil);
+		time(hundredMil);
+
+		
+
+
 	
 	
     } // end main method
