73a9cc8d750c68eab47d776ad628aafa17fd88cc
Raymond Wu <rwu6@stuy.edu>
2018-03-13 23:07:05 -0400

    Fix flaw, create new array on each iteration of for-loop

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -21,21 +21,21 @@ public class QuickSortTester
     } // end time method
     
     public static void main(String[] args)
-    {
-	int[] tenK = QuickSort.buildArray(10000, 100000000);
-	int[] fiftyK = QuickSort.buildArray(50000, 100000000);
-	int[] hundredK = QuickSort.buildArray(100000, 100000000);
-	int[] quarterMil = QuickSort.buildArray(250000, 100000000);
-	int[] halfMil = QuickSort.buildArray(500000, 100000000);
-	int[] tenMil = QuickSort.buildArray(10000000, 100000000);
-	int[] twentyFiveMil = QuickSort.buildArray(25000000, 100000000);
-	int[] fortyMil = QuickSort.buildArray(40000000, 100000000);
-	int[] fiftyMil = QuickSort.buildArray(50000000, 100000000);
-	int[] seventyFiveMil = QuickSort.buildArray(75000000, 100000000);
-	int[] hundredMil = QuickSort.buildArray(100000000, 100000000);
-	
+    {	
 	for (int i = 0; i < 11; i++)
 	    {
+		int[] tenK = QuickSort.buildArray(10000, 100000000);
+		int[] fiftyK = QuickSort.buildArray(50000, 100000000);
+		int[] hundredK = QuickSort.buildArray(100000, 100000000);
+		int[] quarterMil = QuickSort.buildArray(250000, 100000000);
+		int[] halfMil = QuickSort.buildArray(500000, 100000000);
+		int[] tenMil = QuickSort.buildArray(10000000, 100000000);
+		int[] twentyFiveMil = QuickSort.buildArray(25000000, 100000000);
+		int[] fortyMil = QuickSort.buildArray(40000000, 100000000);
+		int[] fiftyMil = QuickSort.buildArray(50000000, 100000000);
+		int[] seventyFiveMil = QuickSort.buildArray(75000000, 100000000);
+		int[] hundredMil = QuickSort.buildArray(100000000, 100000000);
+	
 		time(tenK);
 		time(fiftyK);
 		time(hundredK);
